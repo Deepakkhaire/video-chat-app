104b6ee4d456ed658a01df8709d1d637f1ab7cf1
Deepak Khaire <dkhaire54@gmail.com>
2023-03-20 22:26:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import {Routes , Route} from 'react-router-dom';
+import {Routes , Route, BrowserRouter} from 'react-router-dom';
 
 import HomePages  from './pages/Home'
 import RoomPage from './pages/Room';
 
 function App() {
   return (
+    <BrowserRouter basename='/video-chat-app'>
     <Routes>
       <Route path="/" element={<HomePages />} />
       <Route path="/room/:roomId" element={<RoomPage />} />
     </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
