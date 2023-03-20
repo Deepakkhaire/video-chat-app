c45b9bba6f00a3f36cb3f1df647040865e0a8456
Deepak Khaire <dkhaire54@gmail.com>
2023-03-20 22:29:33 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import RoomPage from './pages/Room';
 
 function App() {
   return (
-    <BrowserRouter basename='/video-chat-app'>
+    
     <Routes>
       <Route path="/" element={<HomePages />} />
       <Route path="/room/:roomId" element={<RoomPage />} />
     </Routes>
-    </BrowserRouter>
+    
   );
 }
 export default App;
