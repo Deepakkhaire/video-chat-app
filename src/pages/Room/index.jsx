950d6be90dfda8c22dd5b2c2e1784baf13ef497d
Deepak Khaire <dkhaire54@gmail.com>
2023-03-20 20:00:00 +0530

    working on to create video-chat-webApp

---
+++ src/pages/Room/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useParams } from 'react-router-dom'
+import {ZegoUIKitPrebuilt} from '@zegocloud/zego-uikit-prebuilt'
+
+const RoomPage = () =>{
+    const { roomId } = useParams();
+
+    const myMeeting = async (ele) => {
+        const appID = 1680348351;
+        const serverSecret = "bb5be9d06ee5a0b4d55adc95005f71be";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Deepak Khaire");
+        const zc = ZegoUIKitPrebuilt.create(kitToken)
+        zc.joinRoom({
+            container : ele,
+            sharedLinks : [
+                {
+                    name: 'Copy Link',
+                    url: 'http:localhost:3000/room/${roomId}'
+                }
+            ],
+            scenario :{
+                mode: ZegoUIKitPrebuilt.OneONoneCall
+            },
+            showScreenSharingButton : true,
+        })
+    }
+    return <div>
+        <div ref={myMeeting} />
+    </div>
+};
+
+export default RoomPage;
